010193fbd9f43f318d23f5d0ca00497181c37161
Cos8o <help.ps.cos8o@gmail.com>
2017-11-26 11:44:51 +0100

    Added things
    Added base for gauntlets and toggle option

--- src/gameUtil.h
+++ src/gameUtil.h
@@ -18,12 +18,25 @@ typedef struct LevelInfo {
 	int demonDiff;
 	int stars;
 };
+typedef struct Gauntlets {
+	std::vector<int> LevelID;
+	std::vector<std::string> levelName;
+	std::vector<std::string> levelAuthor;
+	std::vector<int> difficulty;
+	std::vector<int> isDemon;
+	std::vector<int> isAuto;
+	std::vector<int> demonDiff;
+	std::vector<int> stars;
+};
+extern bool isOnGauntlets;
 bool isPlaying();
 int getBasePointer(int baseOffset);
 char * getDiff(int val, int isAuto, int isDemon, int demonDiff);
 std::string getSmallImageText(int stars, std::string levelName, std::string levelAuthor, int levelID);
 std::string getLeaderboardRank(int accountID);
+std::string getBigImage(bool isOnGauntlets, int gauntletsType);
 LevelInfo getLevelInfo(int levelID);
 std::string getPercentageText(int percentage);
 int getBestPercentage();
+void Hooks_Init();
 #endif
